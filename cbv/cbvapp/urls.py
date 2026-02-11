208540e31d967b4ee8dd8e5c0db8405985eb537b
T Gowtham Reddy <thotamadugula.gowtham@gmail.com>
2026-02-11 21:01:38 +0530

    upadted product details

--- cbv/cbvapp/urls.py
+++ cbv/cbvapp/urls.py
@@ -3,44 +3,21 @@ from cbvapp import views
 
 urlpatterns = [
 
-    # HOME – Company list
+    # ---------------- HOME (Company List) ----------------
     path('', views.AllCompanies.as_view(), name='companies'),
 
-    # EMI (keep above <int:pk>/)
+    # ---------------- EMI ----------------
+    # Keep this above <int:pk>/ pattern
     path('emi/<int:pk>/', views.EmiCalculator.as_view(), name='emi_calculator'),
 
-    # PRODUCT DETAIL
+    # ---------------- PRODUCT DETAIL ----------------
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 
-    # COMPANY CRUD
+    # ---------------- COMPANY CRUD ----------------
     path('add/', views.AddNewCompany.as_view(), name='add_company'),
     path('<int:pk>/edit/', views.EditCompany.as_view(), name='edit_company'),
     path('<int:pk>/delete/', views.DeleteCompany.as_view(), name='delete_company'),
 
-    # INTERIOR
-    path(
-        'product/<int:pk>/interior/add/',
-        views.AddInterior.as_view(),
-        name='add_interior'
-    ),
-    path(
-        'interior/<int:pk>/delete/',
-        views.DeleteInterior.as_view(),
-        name='delete_interior'
-    ),
-
-    # EXTERIOR
-    path(
-        'product/<int:pk>/exterior/add/',
-        views.AddExterior.as_view(),
-        name='add_exterior'
-    ),
-    path(
-    "interior/<int:pk>/delete/",
-    views.DeleteInterior.as_view(),
-    name="delete_interior"
-),
-
-    # COMPANY DETAIL (KEEP LAST)
+    # ---------------- COMPANY DETAIL (KEEP LAST) ----------------
     path('<int:pk>/', views.CompanyDetails.as_view(), name='detail'),
 ]
--- cbv/cbvapp/views.py
+++ cbv/cbvapp/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import get_object_or_404, redirect
-from django.views import View
 from django.views.generic import (
     TemplateView, ListView, DetailView,
     CreateView, UpdateView, DeleteView
 )
 from django.urls import reverse_lazy
-from cbvapp.models import Company, Product, Interior, Exterior
+from cbvapp.models import Company, Product
 
 
 # ---------------- HOME ----------------
@@ -30,20 +28,20 @@ class AddNewCompany(CreateView):
     model = Company
     fields = "__all__"
     template_name = "cbvapp/add_company.html"
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("companies")
 
 
 class EditCompany(UpdateView):
     model = Company
     fields = ["name", "ceo", "est_year", "origin", "image"]
     template_name = "cbvapp/company_Edit.html"
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("companies")
 
 
 class DeleteCompany(DeleteView):
     model = Company
     template_name = "cbvapp/delete_confirm.html"
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("companies")
 
 
 # ---------------- PRODUCT ----------------
@@ -78,55 +76,3 @@ class EmiCalculator(DetailView):
             loan_amount=loan_amount
         )
         return self.render_to_response(context)
-
-
-# ---------------- INTERIOR ----------------
-class AddInterior(CreateView):
-    model = Interior
-    fields = ["image"]
-    template_name = "cbvapp/add_interior.html"
-
-    def form_valid(self, form):
-        form.instance.product_id = self.kwargs["pk"]
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy("product_detail", kwargs={"pk": self.object.product.id})
-
-
-class DeleteInterior(DeleteView):
-    model = Interior
-    template_name = "cbvapp/delete_confirm.html"
-
-    def get_success_url(self):
-        return reverse_lazy("product_detail", kwargs={"pk": self.object.product.id})
-
-
-# ---------------- EXTERIOR ----------------
-class AddExterior(CreateView):
-    model = Exterior
-    fields = ["image"]
-    template_name = "cbvapp/add_exterior.html"
-
-    def form_valid(self, form):
-        form.instance.product_id = self.kwargs["pk"]
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy("product_detail", kwargs={"pk": self.object.product.id})
-
-
-class DeleteInterior(View):
-    def get(self, request, pk):
-        interior = get_object_or_404(Interior, pk=pk)
-        product_id = interior.product.id
-        interior.delete()
-        return redirect("product_detail", pk=product_id)
-
-
-class DeleteExterior(View):
-    def get(self, request, pk):
-        exterior = get_object_or_404(Exterior, pk=pk)
-        product_id = exterior.product.id
-        exterior.delete()
-        return redirect("product_detail", pk=product_id)
